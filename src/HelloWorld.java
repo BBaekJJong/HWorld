e94c0fdcac0aaa73340a07259640d1b0c1f9a129
Administrator <Administrator@43024B01_C0C>
2017-09-22 16:27:18 +0900

    hi world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	
 		System.out.println("HelloWolrd");
-		
-		
+		System.out.println("Hi World");
+		System.out.println("Hi World2");
 	}
 }
